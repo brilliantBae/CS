affec0e824834e7d7712b87a3ba8a1e014fd1e0b
Jiwon Bae <jwb8705@gmail.com>
2018-06-11 16:08:21 +0900

    [Projects] - java-framework Spring IoC, DI 보충

--- designPattern/Singleton.java
+++ designPattern/Singleton.java
@@ -3,8 +3,8 @@ public class Singleton {
 
     private Singleton() {
     }
-
-    public static Singleton getSingletonObject(){
+    // 메소드를 동기화시켜 인스턴스 생성이 여러번 일어나지 않도록 한다.
+    public static synchronized Singleton getSingletonObject(){
         if(singletonObject == null){
             singletonObject = new Singleton();
         }
